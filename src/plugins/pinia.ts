29c8395c24e167bf4e78a8b5f61a6b9bbbf7d3b1
denka <denkaviart.contactme@proton.me>
2026-06-16 20:15:49 +0700

    chore: configure persisted state for pinia

--- src/plugins/pinia.ts
+++ src/plugins/pinia.ts
@@ -1,5 +1,28 @@
 import { createPinia } from 'pinia'
+import SecureLS from 'secure-ls'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+
+interface SecureLSConstructor {
+  new (config?: { isCompression?: boolean }): SecureLS
+}
+
+// Access the constructor safely via .default if it exists
+const SecureConstructor =
+  (SecureLS as unknown as { default: SecureLSConstructor }).default || SecureLS
+
+const ls = new SecureConstructor({
+  isCompression: false,
+})
 
 const pinia = createPinia()
+
+pinia.use(
+  createPersistedState({
+    storage: {
+      getItem: (key: string) => ls.get(key),
+      setItem: (key: string, value: string) => ls.set(key, value),
+    },
+  }),
+)
 
 export default pinia
