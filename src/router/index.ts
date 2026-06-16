ea71703a05c92bf976a75dac059cfc6d42dd9e47
denka <denkaviart.contactme@proton.me>
2026-06-16 20:25:06 +0700

    chore: implement page layout system

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { handleHotUpdate, routes } from 'vue-router/auto-routes'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+
+const appendLayoutToRoutes = (routes: RouteRecordRaw[]) => {
+  return routes.map((route) => {
+    return {
+      path: route.path,
+      component: route.meta?.layout ?? DefaultLayout,
+      children: [
+        {
+          ...route,
+          path: '',
+        },
+      ],
+    }
+  })
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: appendLayoutToRoutes(routes),
 })
 
 // This will update routes at runtime without reloading the page
